9718ed9cee3d35a1ebcf7df0c834c893c0ac4005
Vinny Martinez <bobmeow@users.noreply.github.com>
2016-06-16 17:46:09 -0400

    cracklepop for recurse

---
+++ cracklepop.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+i = 1
+while i <= 100:
+    if i % 3 == 0 and i % 5 == 0:
+        print 'CracklePop'
+    elif i % 3 == 0:
+        print 'Crackle'
+    elif i % 5 == 0:
+        print 'Pop'
+    else:
+        print i
+    i += 1
